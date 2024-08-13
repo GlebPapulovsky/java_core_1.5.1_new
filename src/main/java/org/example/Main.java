0d3bfa28c1a506578bd2973c553e8ef6c7cb316b
dinamic person <papulovskijglech@gmail.com>
2024-08-13 19:19:01 +0300

    parseCSV done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,55 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        System.out.println(Arrays.toString(list.toArray()));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
     }
+
+    private static  List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        List<Employee>list =new ArrayList<>();
+        try (CSVReader reader = new CSVReader(new FileReader("data.csv"))) {
+            // Массив считанных строк
+            String[] nextLine;
+
+            // Читаем CSV построчно
+            while ((nextLine = reader.readNext()) != null) {
+                // Работаем с прочитанными данными.
+                Employee employee=new Employee(
+                        Long.valueOf(nextLine[0]),
+                        nextLine[1],
+                        nextLine[2],
+                        nextLine[3],
+                        Integer.valueOf(nextLine[4])
+                        );
+                list.add(employee);
+
+
+            }
+        } catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
+
+
+
+
+        return list;
+
+    }
+
+
 }
