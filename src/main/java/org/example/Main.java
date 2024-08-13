85c7c29217ac34589d78632e7eaac61ccb09628f
dinamic person <papulovskijglech@gmail.com>
2024-08-13 23:35:35 +0300

    second task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,30 +2,104 @@ package org.example;
 
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvValidationException;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Type;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
+
+        // 1 задание
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
         String fileName = "data.csv";
         List<Employee> list = parseCSV(columnMapping, fileName);
-        System.out.println(Arrays.toString(list.toArray()));
+        //System.out.println(Arrays.toString(list.toArray()));
         String json = listToJson(list);
-        System.out.println(json);
-        writeString(json);
+        //System.out.println(json);
+        writeString(json,"data.json");
+
+        //2 задание
+
+        List<Employee> list2 = parseXML("data.xml");
+        String json2 = listToJson(list);
+        writeString(json2, "data2.json");
+
+
     }
 
-    private static void writeString(String json) {
+    public static List<Employee> parseXML(String filePath) {
+        List<Employee> employees = new ArrayList<>();
+
+        try {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.parse(new File(filePath));
+
+            document.getDocumentElement().normalize();
+
+            NodeList nodeList = document.getElementsByTagName("employee");
+
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element element = (Element) node;
+
+                    long id = Long.parseLong(element.getElementsByTagName("id").item(0).getTextContent());
+                    String firstName = element.getElementsByTagName("firstName").item(0).getTextContent();
+                    String lastName = element.getElementsByTagName("lastName").item(0).getTextContent();
+                    String country = element.getElementsByTagName("country").item(0).getTextContent();
+                    int age = Integer.parseInt(element.getElementsByTagName("age").item(0).getTextContent());
+
+                    employees.add(new Employee(id, firstName, lastName, country, age));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return employees;
+    }
+
+
+
+    public static void writeString(String content, String filePath) {
+        try {
+            Files.write(Paths.get(filePath), content.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static String convertDocumentToString(org.w3c.dom.Document doc) {
+        try {
+            javax.xml.transform.TransformerFactory transformerFactory = javax.xml.transform.TransformerFactory.newInstance();
+            javax.xml.transform.Transformer transformer = transformerFactory.newTransformer();
+            javax.xml.transform.dom.DOMSource source = new javax.xml.transform.dom.DOMSource(doc);
+            java.io.StringWriter writer = new java.io.StringWriter();
+            javax.xml.transform.stream.StreamResult result = new javax.xml.transform.stream.StreamResult(writer);
+            transformer.transform(source, result);
+            return writer.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public static void writerOfString(String json) {
         try (FileWriter writer = new FileWriter("person.json")) {
             writer.write(json);
         } catch (IOException e) {
@@ -58,6 +132,9 @@ public class Main {
     private static String listToJson(List<Employee> list) {
         return new Gson().toJson(list);
     }
-
-
 }
+
+
+
+
+
